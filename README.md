e1d02c6d014ab598b0c4cd889ee08f1e4e78934a
Stan Persoons <spe@cce.be>
2024-12-24 15:13:27 +0100

    v3

--- README.md
+++ README.md
@@ -1,14 +1,91 @@
-# Remove error lines
+# Remove Dart Error/Warning Imports
 
--   Remove error lines from the open file
+The **Remove Dart Error/Warning Imports** extension for Visual Studio Code streamlines your Dart development workflow by automatically removing `import`, `export`, `part`, and `part of` statements that cause errors or warnings in your code.
 
 ---
 
-## How to use
+## Features
 
--   `Ctrl + Shift + P`
-    -   Type: `Remove Error Imports`
+- **Automated Cleanup**: Scans your Dart files and removes lines with `import`, `export`, `part`, and `part of` statements that have associated errors or warnings, helping to maintain clean and error-free code.
 
-Or
+- **User-Friendly Commands**: Easily accessible through the command palette or context menu for quick integration into your development process.
 
--   Right Click on the code
+---
+
+## How to Use
+
+1. **Using Command Palette**:
+
+    - Press `Ctrl + Shift + P` (or `Cmd + Shift + P` on macOS).
+
+    - Type the desired command and select it when it appears:
+
+        - `Remove Dart Error/Warning`
+
+        - `Remove Dart Errors Only`
+
+        - `Remove Dart Warnings Only`
+
+2. **Using Context Menu**:
+
+    - Right-click within the code editor.
+
+    - Select the desired command from the context menu:
+
+        - `Remove Dart Error/Warning`
+
+        - `Remove Dart Errors Only`
+
+        - `Remove Dart Warnings Only`
+
+---
+
+**Note**: The extension uses the following regular expressions to identify and remove lines:
+
+1. **For Import, Export, Part, and Part of Statements**:
+
+   ```regex
+   ^(import|export|part|part of)\s
+   ```
+
+   This regex matches lines that are import, export, part, or part of statements.
+
+2. **For Variable Declarations Starting with `const`, `final`, `var`, or `late`**:
+
+   ```regex
+   ^\s*(const|final|var|late)\s+[\w<>,\s]+\s+\w*\s*(=.*)?\s*;$
+   ```
+
+   This regex matches lines that are variable declarations starting with `const`, `final`, `var`, or `late`.
+
+These regular expressions are used to identify lines that should be removed based on their content and associated diagnostics.
+
+---
+
+## Commands
+
+The extension provides the following commands for flexibility:
+
+- **Remove Dart Error/Warning**: Removes all `import`, `export`, `part`, and `part of` statements with errors or warnings.
+
+- **Remove Dart Errors Only**: Removes only the statements with errors.
+
+- **Remove Dart Warnings Only**: Removes only the statements with warnings.
+
+---
+
+## Installation
+
+To install the extension, visit the [Visual Studio Marketplace](https://marketplace.visualstudio.com/items?itemName=Stan.remove-dart-error-imports) and click 'Install'.
+
+---
+
+## Contributing
+
+Contributions are welcome! If you encounter issues or have suggestions for improvements, please visit the [GitHub repository](https://github.com/stan-at-work/remove-dart-error-warning) to report them or submit pull requests.
+
+---
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](https://github.com/stan-at-work/remove-dart-error-warning/blob/master/LICENSE.md) file for details.
--- out/extension.js
+++ out/extension.js
@@ -3,11 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.activate = void 0;
 const vscode = require("vscode");
 function activate(context) {
-    let disposable = vscode.commands.registerCommand('extension.removeDartImportsWithErrors', () => {
+    const disposable = vscode.commands.registerCommand('extension.removeDartErrorsAndWarnings', () => {
         const editor = vscode.window.activeTextEditor;
         if (editor) {
             const document = editor.document;
-            const updatedText = removeDartImportsWithErrors(document);
+            const updatedText = removeDartErrorsAndWarnings(document, 'all');
             editor.edit((editBuilder) => {
                 const start = new vscode.Position(0, 0);
                 const end = new vscode.Position(document.lineCount + 1, 0);
@@ -16,20 +16,51 @@ function activate(context) {
             });
         }
     });
-    context.subscriptions.push(disposable);
+    const disposable2 = vscode.commands.registerCommand('extension.removeDartOnlyErrors', () => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            const document = editor.document;
+            const updatedText = removeDartErrorsAndWarnings(document, 'error');
+            editor.edit((editBuilder) => {
+                const start = new vscode.Position(0, 0);
+                const end = new vscode.Position(document.lineCount + 1, 0);
+                const range = new vscode.Range(start, end);
+                editBuilder.replace(range, updatedText);
+            });
+        }
+    });
+    const disposable3 = vscode.commands.registerCommand('extension.removeDartOnlyWarnings', () => {
+        const editor = vscode.window.activeTextEditor;
+        if (editor) {
+            const document = editor.document;
+            const updatedText = removeDartErrorsAndWarnings(document, 'warning');
+            editor.edit((editBuilder) => {
+                const start = new vscode.Position(0, 0);
+                const end = new vscode.Position(document.lineCount + 1, 0);
+                const range = new vscode.Range(start, end);
+                editBuilder.replace(range, updatedText);
+            });
+        }
+    });
+    context.subscriptions.push(...[disposable, disposable2, disposable3]);
 }
 exports.activate = activate;
-function removeDartImportsWithErrors(document) {
+function removeDartErrorsAndWarnings(document, type) {
     let updatedText = '';
     for (let line = 0; line < document.lineCount; line++) {
         const lineText = document.lineAt(line).text;
         const diagnostics = vscode.languages.getDiagnostics(document.uri);
-        const lineHasError = diagnostics.some((diagnostic) => diagnostic.range.start.line === line && diagnostic.severity === vscode.DiagnosticSeverity.Error) && /^(import|part|part of)\s*/.test(lineText.trim());
-        if (!lineHasError) {
-            updatedText += lineText;
-            if (line < document.lineCount - 1) {
-                updatedText += '\n';
-            }
+        const isImportExportPartPartOf = /^(import|export|part|part of)\s/.test(lineText.trim());
+        const isVariableDeclaration = /^\s*(const|final|var|late)\s+[\w<>,\s]+\s+\w*\s*(=.*)?\s*;$/.test(lineText.trim());
+        const lineHasError = diagnostics.some((diagnostic) => diagnostic.range.start.line === line && diagnostic.severity === vscode.DiagnosticSeverity.Error);
+        const lineHasWarning = diagnostics.some((diagnostic) => diagnostic.range.start.line === line && diagnostic.severity === vscode.DiagnosticSeverity.Warning);
+        if ((isImportExportPartPartOf || isVariableDeclaration) && lineHasError && (type === 'error' || type === 'all'))
+            continue;
+        if ((isImportExportPartPartOf || isVariableDeclaration) && lineHasWarning && (type === 'warning' || type === 'all'))
+            continue;
+        updatedText += lineText;
+        if (line < document.lineCount - 1) {
+            updatedText += '\n';
         }
     }
     return updatedText;
